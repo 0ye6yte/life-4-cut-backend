3c21e5061d01a7c351121eb5d4d6ad3f3092cc3b
pythonstrup <pythonstrup@gmail.com>
2023-10-12 22:17:23 +0900

    feat: 유저 업데이트 로직 추가

--- src/main/java/com/onebyte/life4cut/user/controller/UserController.java
+++ src/main/java/com/onebyte/life4cut/user/controller/UserController.java
@@ -2,17 +2,23 @@ package com.onebyte.life4cut.user.controller;
 
 import com.onebyte.life4cut.auth.dto.CustomUserDetails;
 import com.onebyte.life4cut.common.web.ApiResponse;
+import com.onebyte.life4cut.user.controller.dto.UpdateUserRequest;
 import com.onebyte.life4cut.user.controller.dto.UserDuplicateResponse;
 import com.onebyte.life4cut.user.controller.dto.UserFindResponse;
 import com.onebyte.life4cut.user.domain.User;
 import com.onebyte.life4cut.user.service.UserService;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
@@ -33,11 +39,19 @@ public class UserController {
     return ApiResponse.OK(UserFindResponse.of(result));
   }
 
+  @PatchMapping("/me")
+  @ResponseStatus(value = HttpStatus.NO_CONTENT)
+  public void updateMe(
+      @AuthenticationPrincipal CustomUserDetails user,
+      @RequestPart(value = "data", required = false) UpdateUserRequest request,
+      @RequestPart(value = "image", required = false) MultipartFile image) {
+    userService.updateUser(user.getUserId(), request, image);
+  }
+
   @GetMapping("/duplicate")
-  public ApiResponse<UserDuplicateResponse> checkDuplicatedNickname(@RequestParam("nickname") String nickname) {
+  public ApiResponse<UserDuplicateResponse> checkDuplicatedNickname(
+      @RequestParam("nickname") String nickname) {
     Optional<User> result = userService.findUserByNicknameForCheckingDuplication(nickname);
-    return ApiResponse.OK(
-        UserDuplicateResponse.of(result)
-    );
+    return ApiResponse.OK(UserDuplicateResponse.of(result));
   }
 }
--- src/main/java/com/onebyte/life4cut/user/service/UserService.java
+++ src/main/java/com/onebyte/life4cut/user/service/UserService.java
@@ -1,6 +1,11 @@
 package com.onebyte.life4cut.user.service;
 
 import com.onebyte.life4cut.auth.dto.OAuthInfo;
+import com.onebyte.life4cut.common.constants.S3Env;
+import com.onebyte.life4cut.support.fileUpload.FileUploadResponse;
+import com.onebyte.life4cut.support.fileUpload.FileUploader;
+import com.onebyte.life4cut.support.fileUpload.MultipartFileUploadRequest;
+import com.onebyte.life4cut.user.controller.dto.UpdateUserRequest;
 import com.onebyte.life4cut.user.controller.dto.UserSignInRequest;
 import com.onebyte.life4cut.user.domain.User;
 import com.onebyte.life4cut.user.exception.UserNotFound;
@@ -10,22 +15,28 @@ import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+@Transactional
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
   private final UserRepository userRepository;
+  private final FileUploader fileUploader;
+  private final S3Env s3Env;
 
   public User save(UserSignInRequest request) {
     return userRepository.save(request.toEntity());
   }
 
+  @Transactional(readOnly = true)
   public User findUser(long id) {
-    return userRepository.findUser(id)
-        .orElseThrow(UserNotFound::new);
+    return userRepository.findUser(id).orElseThrow(UserNotFound::new);
   }
 
+  @Transactional(readOnly = true)
   public Optional<User> findUserByOAuthInfo(OAuthInfo oAuthInfo) {
     List<User> result = userRepository.findUserByOAuthInfo(oAuthInfo);
     if (result.size() > 1) {
@@ -34,11 +45,27 @@ public class UserService {
     return result.stream().findAny();
   }
 
+  @Transactional(readOnly = true)
   public User findUserByNickname(String nickname) {
     return userRepository.findUserByNickname(nickname).orElseThrow(UserNotFound::new);
   }
 
+  @Transactional(readOnly = true)
   public Optional<User> findUserByNicknameForCheckingDuplication(String nickname) {
     return userRepository.findUserByNickname(nickname);
+  }
+
+  public void updateUser(long id, UpdateUserRequest request, MultipartFile image) {
+    User user = userRepository.findUser(id).orElseThrow(UserNotFound::new);
+    user.changeNickname(request.nickname());
+
+    if (image == null) {
+      return;
+    }
+
+    FileUploadResponse response =
+        fileUploader.upload(MultipartFileUploadRequest.of(image, s3Env.bucket()));
+    String imagePath = response.key();
+    user.changeProfilePath(imagePath);
   }
 }
